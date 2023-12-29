abe4657381ac8cc61e5dd4aa207ee8c35830be84
Matroskin <matroskin771@mail.ru>
2023-12-29 17:05:28 +0300

    fix json corner

--- internals/util/structJson.go
+++ internals/util/structJson.go
@@ -1,17 +1,17 @@
 package util
 
 type BodyHTTP struct { //TODO(N): remake
-	Points                []Point          `json:"points"`                // Коордианты точек. Последовательность важна, влияет ниже
-	Lines                 []PairNumber     `json:"lines"`                 // Номера точек
-	EqualTwoPoints        []PairNumber     `json:"equalTwoPoints"`        // Номера точек
-	DistanceBetweenPoints []DistancePoints `json:"distanceBetweenPoints"` // Номера точек
-	FixationPoint         []int            `json:"fixationPoint"`         // Массив из номеров точек, которые зафиксированы
-	BelongOfLine          []PairNumber     `json:"belongOfLine"`          // Первый - номер точки, второй - номер линии
-	ParallelTwoLines      []PairNumber     `json:"parallelTwoLines"`      // Номера линий
-	PerpenTwoLines        []PairNumber     `json:"perpenTwoLines"`        // Номера линий
-	CornerTwoLines        []PairNumber     `json:"cornerTwoLines"`        // Номера линий
-	VerticalLine          []int            `json:"verticalLine"`          // Массив из номеров линий
-	HorizontLine          []int            `json:"horizontLine"`          // Массив из номеров линий
+	Points                []Point            `json:"points"`                // Коордианты точек. Последовательность важна, влияет ниже
+	Lines                 []PairNumber       `json:"lines"`                 // Номера точек
+	EqualTwoPoints        []PairNumber       `json:"equalTwoPoints"`        // Номера точек
+	DistanceBetweenPoints []DistancePoints   `json:"distanceBetweenPoints"` // Номера точек
+	FixationPoint         []int              `json:"fixationPoint"`         // Массив из номеров точек, которые зафиксированы
+	BelongOfLine          []PairNumber       `json:"belongOfLine"`          // Первый - номер точки, второй - номер линии
+	ParallelTwoLines      []PairNumber       `json:"parallelTwoLines"`      // Номера линий
+	PerpenTwoLines        []PairNumber       `json:"perpenTwoLines"`        // Номера линий
+	CornerTwoLines        []PairNumberCorner `json:"cornerTwoLines"`        // Номера линий
+	VerticalLine          []int              `json:"verticalLine"`          // Массив из номеров линий
+	HorizontLine          []int              `json:"horizontLine"`          // Массив из номеров линий
 }
 
 type Point struct {
@@ -22,6 +22,12 @@ type Point struct {
 type PairNumber struct {
 	First  int `json:"first"`
 	Second int `json:"second"`
+}
+
+type PairNumberCorner struct {
+	First  int `json:"first"`
+	Second int `json:"second"`
+	Value  int `json:"value"`
 }
 
 type DistancePoints struct {
